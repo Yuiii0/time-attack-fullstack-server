6fa3896e477f30ddedde84e3c4ae7924393630f0
Yujiii0 <yuji918@naver.com>
2024-03-05 11:53:54 +0900

    deploy

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,13 @@ import { TransformInterceptor } from './intercepters/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-      credentials: true,
-    }),
-  );
+  // app.use(
+  //   cors({
+  //     origin: 'http://localhost:3000',
+  //     credentials: true,
+  //   }),
+  // );
+  app.use(cors());
   app.use(cookieParser());
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
